09b66a5e73fed4a0208f93f96027bf48aaa4addb
TheRealTalkingBen <rebence2006@gmail.com>
2026-01-09 19:54:53 +0100

    Updated packages,  added LoginForm, HomePage, Theme

---
+++ src/components/LoginNavbar.tsx
@@ -0,0 +1,11 @@
+import Theme from "./Theme";
+
+export default function LoginNavbar() {
+    return (
+        <div className="navbar bg-base-200 shadow-xs">
+            <div className="flex-none">
+                <Theme />
+            </div>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import LoginForm from './components/LoginForm.tsx'
+import Home from './Pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<LoginForm />} />
       <Route path='/test' element={<p>Test</p>} />
     </Routes>
   </BrowserRouter>,
