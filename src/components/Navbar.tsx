85dea0271dac4dbc843a68a08ad178ba2c82ea09
TheRealTalkingBen <rebence2006@gmail.com>
2026-01-14 20:56:55 +0100

    Dropdown menu fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
-                <div className="dropdown hidden lg:block">
+                <div className="dropdown lg:hidden">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                     </div>
@@ -17,17 +17,10 @@ export default function Navbar() {
                 </div>
                 <Theme />
             </div>
-            <div className="navbar-center flex lg:hidden">
+            <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <details>
-                            <summary>Menu</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/about">About</a></li>
-                            </ul>
-                        </details>
-                    </li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
