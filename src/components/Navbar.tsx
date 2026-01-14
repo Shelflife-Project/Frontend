8ae398338af8f7c78469f9874f883e609e9a1a4b
TheRealTalkingBen <rebence2006@gmail.com>
2026-01-14 20:31:05 +0100

    Different dropdown menu for mobile and computer users

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,31 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
-                <div className="dropdown">
+                <div className="dropdown hidden lg:block">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                     </div>
                     <ul
                         tabIndex={-1}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a href="/">Homepage</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                     </ul>
                 </div>
                 <Theme />
+            </div>
+            <div className="navbar-center flex lg:hidden">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <details>
+                            <summary>Menu</summary>
+                            <ul className="p-2 bg-base-100 w-40 z-1">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/about">About</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
             </div>
             <div className="navbar-end">
                 <a href="/login" className="btn btn-secondary">Login</a>
