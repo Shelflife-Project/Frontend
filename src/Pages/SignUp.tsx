28e89cfb38449b571edbe97a49da9b4de327e891
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-01-16 17:59:15 +0100

    added #18

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router";
 import Navbar from "../components/Navbar"
 import SignUpForm from "../components/SignUpForm"
+import { getCurrentUser } from "../Fetch_APIs/autentication";
+import { useEffect } from "react";
 
 export default function SignUp() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getCurrentUser()
+            .then(user => {
+                if (user)
+                    navigate("/dashboard", { replace: true })
+            });
+    }, []);
+
     return (
         <>
             <Navbar />
