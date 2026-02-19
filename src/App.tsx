8ddac0cf09a3a7414b5565125b70e423643f1f93
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-02-19 09:19:14 +0100

    refactored signup and user icon

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { useEffect } from "react";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
     const { getMe } = useAuth();
@@ -21,13 +23,12 @@ export default function App() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            {
-                /*<Route path='/about' element={<About />} />
-                <Route path='/signup' element={<SignUp />} />
-                <Route path='/dashboard/*' element={<ProtectedRoute element={<Dashboard />} />} />
-                */
-            }
+            <Route path='/about' element={<About />} />
             <Route path='/login' element={<UnProtectedRoute element={<Login />} />} />
+            <Route path='/signup' element={<UnProtectedRoute element={<SignUp />} />} />
+            {
+                //<Route path='/dashboard/*' element={<ProtectedRoute element={<Dashboard />} />} />
+            }
             <Route path='/dashboard/*' element={<ProtectedRoute element={<Navbar />} />} />
         </Routes>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AuthProvider } from 'shelflife-react-hooks'
+import { AuthProvider, UserProvider } from 'shelflife-react-hooks'
 import { localStorageAdapter } from './LocalStorageAdapter.ts'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
@@ -9,8 +9,10 @@ import { StrictMode } from 'react'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider baseUrl={'http://localhost:8080'} tokenStorage={localStorageAdapter}>
-        <App />
+      <AuthProvider baseUrl={'http://localhost:8080'} tokenStorage={localStorageAdapter} initialToken={await localStorageAdapter.getItem("auth_token")}>
+        <UserProvider baseUrl={'http://localhost:8080'}>
+          <App />
+        </UserProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
