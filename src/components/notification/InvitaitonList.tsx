f5c6528893476d9b8f62543e05487c7b03940316
andraspreisler <preisler.andras@gmail.com>
2026-02-22 11:05:02 +0100

    Refactor InvitationList and ShoppingList components to improve invite handling and clean up unused code

--- src/components/notification/InvitaitonList.tsx
+++ src/components/notification/InvitaitonList.tsx
@@ -1,55 +1,46 @@
+import { useEffect } from "react";
+import { useInvites } from "shelflife-react-hooks";
+
 type Prop = {
 
 }
 
 export default function InvitationList({ }: Prop) {
+    const { fetchInvites, declineInvite, acceptInvite, invites, isLoading, isError } = useInvites();
+   
+    useEffect(() => {
+        if (invites.length > 0 || isLoading || isError) return;
+        fetchInvites();
+    }, []);
+    
+
+    if (isLoading) {
+        return <div>Loading invitations...</div>;
+    }
+
+    if (isError) {
+        return <div>Error loading invitations.</div>;
+    }
+
     return (
         <ul className="list bg-base-300 rounded-box shadow-md">
 
             <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">Invitations for you</li>
 
-            <li className="list-row">
-                <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp" /></div>
-                <div>
-                    <div>Dio Lupa</div>
-                    <div className="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
-                </div>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                </button>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                </button>
-            </li>
-
-            <li className="list-row">
-                <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp" /></div>
-                <div>
-                    <div>Ellie Beilish</div>
-                    <div className="text-xs uppercase font-semibold opacity-60">Bears of a fever</div>
-                </div>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                </button>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                </button>
-            </li>
-
-            <li className="list-row">
-                <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/3@94.webp" /></div>
-                <div>
-                    <div>Sabrino Gardener</div>
-                    <div className="text-xs uppercase font-semibold opacity-60">Cappuccino</div>
-                </div>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                </button>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                </button>
-            </li>
-
+            {invites.filter(invite => !invite.accepted).map((invite) => (
+                <li key={invite.id} className="list-row items-center">
+                    <div>
+                        <div>You have been invited to <span className="font-semibold">{invite.storage.name}</span>, </div>
+                        <div className="text-xs uppercase font-semibold opacity-60">{invite.storage.owner.username.substring(0, 15)} wants to share this storage with you.</div>
+                    </div>
+                    <button className="btn  btn-sm sm:btn-md btn-success" onClick={() => acceptInvite(invite.id)}>
+                        <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M5 13l4 4L19 7"></path></g></svg>
+                    </button>  
+                    <button className="btn btn-sm sm:btn-md btn-error" onClick={() => declineInvite(invite.id)}>
+                        <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M18 6L6 18M6 6l12 12"></path></g></svg>
+                    </button>  
+                </li>
+            ))}
         </ul>
     );
 }
