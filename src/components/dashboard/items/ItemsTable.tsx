42ab9f2b978bb852294a83196ce8661f9d6934ab
TheRealTalkingBen <rebence2006@gmail.com>
2026-03-02 12:00:02 +0100

    item table grouping

--- src/components/dashboard/items/ItemsTable.tsx
+++ src/components/dashboard/items/ItemsTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStorageItems, type StorageItem } from "shelflife-react-hooks";
 import FormPopUp from "../../FormPopUp";
 import EditStorageItemsForm from "./EditStorageItemForm";
@@ -9,6 +9,7 @@ type Props = {
 
 export default function ItemsTable({ storageId }: Props) {
     const { items, fetchItems, deleteItem } = useStorageItems();
+    const [itemsTable, setItemsTable] = useState<Map<number, StorageItem[]>>(new Map<number, StorageItem[]>());
 
     const deleteItemHandler = async (item_id: number) => {
         const confirmDelete = confirm("Are you sure you want to remove this item?");
@@ -25,35 +26,64 @@ export default function ItemsTable({ storageId }: Props) {
         return diffDays;
     };
 
+    const groupItems = async () => {
+        let table = new Map<number, StorageItem[]>();
+
+        items.forEach((x) => {
+            if (table.has(x.product.id)) {
+                let list = table.get(x.product.id)!;
+
+                list.push(x);
+                table.set(x.product.id, list);
+            }
+            else {
+                table.set(x.product.id, [x]);
+            }
+        })
+
+        setItemsTable(table);
+    };
+
+    useEffect(() => {
+        groupItems();
+    }, [items])
+
     useEffect(() => {
         fetchItems(storageId);
     }, [storageId]);
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mx-auto flex justify-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mx-auto flex justify-items-center">
             {
-                items.map((x, i) =>
-                    <div key={i} className="card bg-base-300 shadow-sm">
+                [...itemsTable].map(([key, value]) => (
+                    <div key={key} className="card bg-base-300 max-w-82 shadow-sm">
                         <figure>
                             <img
-                                src={`${import.meta.env.VITE_BACKEND_BASE_URL}/api/products/${x.id}/icon`}
+                                src={`${import.meta.env.VITE_BACKEND_BASE_URL}/api/products/${key}/icon`}
                                 alt="Shoes" />
                         </figure>
                         <div className="card-body">
-                            <h2 className="card-title">{x.product.name}</h2>
-                            <h2
-                                className={daysToExpire(x) < 0 ? "text-error" : daysToExpire(x) < 2 ? "text-warning" : ""}>
-                                Expires at: {new Date(x.expiresAt).toLocaleDateString()}
-                            </h2>
-                            <div className="card-actions justify-between">
-                                <button className="btn btn-error" onClick={() => deleteItemHandler(x.id)}>Delete</button>
-                                <FormPopUp button={<button className="btn btn-primary">Edit</button>}>
-                                    <EditStorageItemsForm storageId={storageId} itemId={x.id} />
-                                </FormPopUp>
-                            </div>
+                            <h2 className="card-title">{value[0].product.name}</h2>
+
+                            {
+                                value.map((y, i) =>
+                                    <div className="gap-4 flex justify-between">
+
+                                        <h2
+                                            key={i}
+                                            className={daysToExpire(y) < 0 ? "text-error" : daysToExpire(y) < 2 ? "text-orange-600" : ""}>
+                                            Expires at: {new Date(y.expiresAt).toLocaleDateString()}
+                                        </h2>
+                                        <FormPopUp button={<button className="btn btn-primary">Edit</button>}>
+                                            <EditStorageItemsForm storageId={storageId} itemId={y.id} />
+                                        </FormPopUp>
+                                        <button className="btn btn-error" onClick={() => deleteItemHandler(y.id)}>Delete</button>
+                                    </div>
+                                )
+                            }
                         </div>
                     </div>
-                )
+                ))
             }
         </div>
     );
