c5f82b5071279c71587607c69cea69bf76b4662c
TheRealTalkingBen <rebence2006@gmail.com>
2026-03-08 13:54:06 +0100

    add EditStorageNameForm to settings

--- src/pages/dashboard/Settings.tsx
+++ src/pages/dashboard/Settings.tsx
@@ -1,21 +1,27 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import SettingsTable from "../../components/dashboard/settings/SettingsTable";
 import FormPopUp from "../../components/FormPopUp";
 import { CreateButtonWithOutClick } from "../../components/dashboard/CreateButton";
 import CreateSettingForm from "../../components/dashboard/settings/CreateSettingForm";
+import EditStorageNameForm from "../../components/dashboard/settings/EditStorageNameForm";
 
 export default function Settings() {
     const { id } = useParams();
 
     return (
-        <>
-            <div className="p-4 container mx-auto">
-                <h1 className="text-3xl font-bold mb-4 text-center">Settings</h1>
-                <SettingsTable storageId={Number(id)} />
-                <FormPopUp button={<CreateButtonWithOutClick />}>
-                    <CreateSettingForm storageId={Number(id)} />
-                </FormPopUp>
+        <div className="container mx-auto p-8">
+            <h1 className="text-3xl font-bold text-center">Settings</h1>
+
+            <Link to={"/dashboard"} className="btn btn-secondary">Back</Link>
+
+            <div className="my-4">
+                <EditStorageNameForm storageId={Number(id)} />
             </div>
-        </>
+
+            <SettingsTable storageId={Number(id)} />
+            <FormPopUp button={<CreateButtonWithOutClick />}>
+                <CreateSettingForm storageId={Number(id)} />
+            </FormPopUp>
+        </div>
     )
 }
