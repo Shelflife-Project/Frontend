a72983d370821f9bc0565e790abe3df984820c2e
Tibor <185492254+Rocka06@users.noreply.github.com>
2026-03-08 21:31:51 +0100

    the product table is now made out of cards; added the ability to upload icons

--- src/components/dashboard/products/ProductTable.tsx
+++ src/components/dashboard/products/ProductTable.tsx
@@ -1,19 +1,35 @@
-import { useEffect, useMemo, useState } from "react";
-import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable, type ColumnDef, type Row, type SortingState } from "@tanstack/react-table";
-import { useAuth, useProducts, type Product } from "shelflife-react-hooks";
+import { useEffect, useState, type ChangeEvent } from "react";
 import FormPopUp from "../../FormPopUp";
 import EditProductForm from "./EditProductForm";
+import { useAuth, useProducts, type Product } from "shelflife-react-hooks";
 
 
 export default function ProductTable() {
-    const [sorting, setSorting] = useState<SortingState>([]);
-    const [globalFilter, setGlobalFilter] = useState("");
+    const { products, fetchProducts, deleteProduct, uploadProductIcon } = useProducts();
     const { user } = useAuth();
-    const { products, fetchProducts, deleteProduct } = useProducts();
+    const [refreshKey, setRefreshKey] = useState<number>(0);
 
     useEffect(() => {
         fetchProducts();
     }, [])
+
+    const canEdit = (product: Product) => {
+        if (user?.admin)
+            return true;
+
+        return product.ownerId === user?.id;
+    }
+
+    const handleIconChange = async (e: ChangeEvent<HTMLInputElement>, product: Product) => {
+        if (!product)
+            return;
+
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        await uploadProductIcon(product.id, file);
+        setRefreshKey((prevKey) => prevKey + 1);
+    };
 
     const deleteProductHandler = async (id: number) => {
         const confirmDelete = confirm("Are you sure you want to delete this product?");
@@ -23,137 +39,73 @@ export default function ProductTable() {
         }
     };
 
-    const columns = useMemo<ColumnDef<Product>[] | any>(() => [
-        {
-            accessorKey: "name",
-            header: "Name",
-        },
-        {
-            accessorKey: "category",
-            header: "Category",
-        },
-        {
-            accessorKey: "barcode",
-            header: "Barcode",
-        },
-        {
-            accessorKey: "expirationDaysDelta",
-            header: "Expires In (Days)"
-        },
-        {
-            header: "Actions",
-            enableSorting: false,
-            enableGlobalFilter: false,
-            cell: ({ row }: { row: Row<Product> }) => {
-                const product = row.original;
-                if (product.ownerId === user?.id)
-                    return (
-                        <div className="grid grid-cols-2 gap-2 min-w-max">
-                            <FormPopUp button={<button className="btn btn-sm w-full btn-primary">Edit</button>} >
-                                <EditProductForm productId={product.id} />
-                            </FormPopUp>
-                            <button className="btn btn-sm btn-error" onClick={() => deleteProductHandler(product.id)}>
-                                Delete
-                            </button>
-                        </div>
-                    );
-
-                return null;
-            },
-        }
-    ], [])
-
-    const table = useReactTable({
-        data: products,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getFilteredRowModel: getFilteredRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        onGlobalFilterChange: setGlobalFilter,
-        onSortingChange: setSorting,
-        state: { sorting, globalFilter },
-        initialState: {
-            pagination: {
-                pageSize: 10,
-            },
-        },
-    });
-
     return (
-        <>
-            <input type="text" onChange={e => table.setGlobalFilter(e.target.value)} className="input input-bordered mb-2 w-fit" placeholder="Search" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mx-auto flex justify-items-center">
+            {
+                products.map((p) => (
+                    <div key={p.id} className="card bg-base-300 max-w-82 shadow-sm">
+                        {
+                            canEdit(p) &&
+                            <figure>
+                                <div className="relative group">
 
-            <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 mb-2">
-                <table className="table">
-                    <thead>
-                        {table.getHeaderGroups().map(headerGroup => (
-                            <tr key={headerGroup.id}>
-                                {headerGroup.headers.map(header => (
-                                    <th key={header.id} className="cursor-pointer" onClick={header.column.getToggleSortingHandler()}>
-                                        <div className="space-x-2">
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                            {header.column.getIsSorted() === "asc" && "↑"}
-                                            {header.column.getIsSorted() === "desc" && "↓"}
+                                    <input
+                                        id={`fileInput-${p.id}`}
+                                        type="file"
+                                        accept="image/*"
+                                        className="hidden"
+                                        onChange={(e) => handleIconChange(e, p)}
+                                    />
+
+                                    <label htmlFor={`fileInput-${p.id}`} className="cursor-pointer block">
+                                        <img
+                                            src={`${import.meta.env.VITE_BACKEND_BASE_URL}/api/products/${p.id}/icon?${refreshKey}`}
+                                            className="rounded-lg"
+                                        />
+
+                                        <label className="w-max absolute bottom-2 right-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera" viewBox="0 0 16 16">
+                                                <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z" />
+                                                <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0" />
+                                            </svg>
+                                        </label>
+
+                                        <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center text-white text-sm transition">
+                                            Change icon
                                         </div>
-                                    </th>
-                                ))}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody>
-                        {table.getRowModel().rows.map(row => (
-                            <tr key={row.id}>
-                                {row.getVisibleCells().map(cell => (
-                                    <td key={cell.id}>
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext()
-                                        )}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                                    </label>
 
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <button
-                        className="btn btn-sm"
-                        onClick={() => table.previousPage()}
-                        disabled={!table.getCanPreviousPage()}
-                    >
-                        {"<"}
-                    </button>
-                    <button
-                        className="btn btn-sm"
-                        onClick={() => table.nextPage()}
-                        disabled={!table.getCanNextPage()}
-                    >
-                        {">"}
-                    </button>
-                    <p className="text-sm select-none">
-                        {"Page " + (table.getState().pagination.pageIndex + 1) + " of " + table.getPageCount()}
-                    </p>
-                </div>
+                                </div>
+                            </figure>
+                        }
 
-                <select
-                    className="select select-sm w-fit"
-                    value={table.getState().pagination.pageSize}
-                    onChange={e => table.setPageSize(Number(e.target.value))}
-                >
-                    {[10, 25, 50, 100].map(size => (
-                        <option key={size} value={size}>
-                            Show {size}
-                        </option>
-                    ))}
-                </select>
-            </div>
-        </>
+                        {
+                            !canEdit(p) &&
+                            <img
+                                src={`${import.meta.env.VITE_BACKEND_BASE_URL}/api/products/${p.id}/icon`}
+                                className="rounded-lg"
+                            />
+                        }
+
+                        <div className="card-body">
+                            <h2 className="card-title">{p.name}</h2>
+                            <h2>Category: {p.category}</h2>
+                            <h2>Barcode: {p.barcode}</h2>
+                            <h2>Expiration delta: {p.expirationDaysDelta} days</h2>
+
+                            {
+                                canEdit(p) &&
+                                <div className="flex justify-between mt-auto">
+                                    <button className="btn btn-error" onClick={() => deleteProductHandler(p.id)}>Delete</button>
+                                    <FormPopUp button={<button className="btn btn-primary">Edit</button>}>
+                                        <EditProductForm productId={p.id} />
+                                    </FormPopUp>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
--- src/pages/dashboard/Products.tsx
+++ src/pages/dashboard/Products.tsx
@@ -5,7 +5,7 @@ import FormPopUp from "../../components/FormPopUp";
 
 export default function Products() {
     return (
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto p-4 mb-8">
             <div className="mb-8 text-center">
                 <h1 className="text-3xl font-bold mb-4">Products</h1>
                 <p className="text-gray-600">View and manage your products here.</p>
