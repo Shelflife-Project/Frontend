fc271adbc0231b1ca01e6c470c17aa76ae9c5e8a
TheRealTalkingBen <rebence2006@gmail.com>
2026-03-12 14:17:39 +0100

    fixed update product doesnt update list

--- src/components/dashboard/products/ProductTable.tsx
+++ src/components/dashboard/products/ProductTable.tsx
@@ -3,20 +3,18 @@ import { useProducts, type Product } from "shelflife-react-hooks";
 import ProductCard from "./ProductCard";
 
 export default function ProductTable() {
-    const { fetchProducts } = useProducts();
+    const { products, fetchProducts } = useProducts();
 
-    const [currentProducts, setCurrentProducts] = useState<Product[]>([]);
     const [nextProducts, setNextProducts] = useState<Product[]>([]);
     const [search, setSearch] = useState("");
     const [page, setPage] = useState(0);
     const [pageSize, setPageSize] = useState(5);
 
     const getProducts = async () => {
-        const current = await fetchProducts(search, pageSize, page);
         const next = await fetchProducts(search, pageSize, page + 1);
-
-        setCurrentProducts(current);
         setNextProducts(next);
+
+        fetchProducts(search, pageSize, page);
     };
 
     useEffect(() => {
@@ -75,18 +73,20 @@ export default function ProductTable() {
                 </div>
             </div>
 
-            {currentProducts.length === 0 && (
+            {products.length === 0 && (
                 <p className="text-center text-gray-400">
                     No products created yet. Click the + button to create one!
                 </p>
             )}
 
-            {currentProducts.length > 0 && (
+            {products.length > 0 && (
                 <>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-6 mx-auto justify-items-center">
-                        {currentProducts.map((p) => (
-                            <ProductCard key={p.id} product={p} />
-                        ))}
+                        {
+                            products.map((p) => (
+                                <ProductCard key={p.id} product={p} />
+                            ))
+                        }
                     </div>
                 </>
             )}
