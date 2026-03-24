bd7e595d11325ebc2ee42af5e171a484ba219d68
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-03-24 19:08:15 +0100

    added tooltips #141

--- src/components/dashboard/notification/RunningLowList.tsx
+++ src/components/dashboard/notification/RunningLowList.tsx
@@ -101,13 +101,19 @@ export default function RunningLowList() {
                                             Added ✓
                                         </span>
                                     ) : (
-                                        <button onClick={() => onAddToCart(item)} className="btn btn-square btn-success p-2">
+                                        <button
+                                            title={`Add ${item.runningLowAt - item.amount + 1} of ${item.product.name} to the shopping list`}
+                                            onClick={() => onAddToCart(item)}
+                                            className="btn btn-square btn-success p-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
                                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                                             </svg>
                                         </button>
                                     )}
-                                    <Link to={`/dashboard/storages/${item.storage.id}/settings`} className="btn btn-square btn-primary p-2">
+                                    <Link
+                                        title="Edit Rule"
+                                        to={`/dashboard/storages/${item.storage.id}/settings`}
+                                        className="btn btn-square btn-primary p-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                             <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
