fc80a548c9af9b7639178ae936621174c3e5c0fa
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-04-16 09:56:35 +0200

    installed react-lottie-player and added some animations

--- src/pages/dashboard/Storages.tsx
+++ src/pages/dashboard/Storages.tsx
@@ -1,12 +1,22 @@
 import StorageList from "../../components/dashboard/storage/StorageList";
+import Inventory from "../../assets/lotties/Inventory.json";
+import BetterLottie from "../../components/BetterLottie";
 
 export default function Storages() {
     return (
-        <div className="p-8 text-center">
-            <h1 className="text-3xl font-bold mb-4">Storages</h1>
-            <p className="text-gray-600 mb-8">Manage your storages here</p>
+        <div className="p-8 relative">
+            <div className="mb-4">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold mb-4">Storages</h1>
+                    <p className="text-gray-600">Manage your storages here</p>
+                </div>
+
+                <div className="hidden sm:flex absolute top-5 right-0 w-64">
+                    <BetterLottie animationData={Inventory} />
+                </div>
+            </div>
 
             <StorageList />
         </div>
-    )
+    );
 }
