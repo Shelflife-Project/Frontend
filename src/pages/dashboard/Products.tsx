13ab97862a36f0d39b3a57e51d8de4d009757e9a
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-04-16 11:06:29 +0200

    added 2 more lotties

--- src/pages/dashboard/Products.tsx
+++ src/pages/dashboard/Products.tsx
@@ -1,12 +1,21 @@
+import BetterLottie from "../../components/BetterLottie";
 import ProductTable from "../../components/dashboard/products/ProductTable";
+import Grocery from "../../assets/lotties/Grocery.json";
 
 export default function Products() {
     return (
-        <div className="mx-auto p-8 mb-8">
-            <div className="mb-8 text-center">
-                <h1 className="text-3xl font-bold mb-4">Product Database</h1>
-                <p className="text-gray-600">View and manage products here.</p>
+        <div className="p-8 relative">
+            <div className="mb-4">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold mb-4">Product Database</h1>
+                    <p className="text-gray-600">View and manage products here.</p>
+                </div>
+
+                <div className="hidden sm:flex absolute top-0 right-0 w-48">
+                    <BetterLottie animationData={Grocery} />
+                </div>
             </div>
+
             <ProductTable />
         </div>
     )
